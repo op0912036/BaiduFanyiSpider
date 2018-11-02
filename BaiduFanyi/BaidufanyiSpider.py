11494c23768d7c486bd99d3dbec50fd63a6b9732
程序猿 <op0912036@163.com>
2018-11-02 21:27:05 +0800

    增加代理

--- BaiduFanyi/BaidufanyiSpider.py
+++ BaiduFanyi/BaidufanyiSpider.py
@@ -17,16 +17,16 @@ class BaiduFanyiSpider:
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
             "Cookie": "BAIDUID=A0CB0FE1DB6D18809C712B5062BBAC6F:FG=1; BIDUPSID=A0CB0FE1DB6D18809C712B5062BBAC6F; PSTM=1538210057; REALTIME_TRANS_SWITCH=1; FANYI_WORD_SWITCH=1; HISTORY_SWITCH=1; SOUND_SPD_SWITCH=1; SOUND_PREFER_SWITCH=1; Hm_lvt_afd111fa62852d1f37001d1f980b6800=1539151103,1539251833; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; locale=zh; pgv_pvi=4554512384; delPer=0; H_PS_PSSID=1438_21082_27401_26350; Hm_lvt_64ecd82404c51e03dc91cb9e8c025574=1541036302,1541037057,1541040002,1541054643; PSINO=5; Hm_lpvt_64ecd82404c51e03dc91cb9e8c025574=1541057937; to_lang_often=%5B%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%2C%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%5D; from_lang_often=%5B%7B%22value%22%3A%22jp%22%2C%22text%22%3A%22%u65E5%u8BED%22%7D%2C%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%2C%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%5D"
         }
-        # self.proxies = {
-        #     "https": "https://202.112.237.102:3128"
-        # }
+        self.proxies = {
+            "https": "https://202.112.237.102:3128"
+        }
 
     def parse_url(self, url, params=None, data=None, method='get'):
         """解析url，获取响应内容"""
         if method == 'get':
-            resp = requests.get(url=url, params=params, headers=self.headers)
+            resp = requests.get(url=url, params=params, headers=self.headers, proxies=self.proxies)
         else:
-            resp = requests.post(url=url, data=data, headers=self.headers)
+            resp = requests.post(url=url, data=data, headers=self.headers, proxies=self.proxies)
 
         return resp.content.decode('utf-8')
 
